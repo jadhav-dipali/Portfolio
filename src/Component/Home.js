41f9883ada2575c68e730f92708d662f5d2dd312
jadhav-dipali <jadhavdipali924@gmail.com>
2023-06-23 16:48:47 +0530

    skill file add

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -13,9 +13,9 @@ export default function Home(){
         <h1 id="hey-i-am-home">Hey ! I Am </h1>
         <h1 id="name-home-at-name">Dipali Jadhav</h1>
         <p id="info-about-me-in-small">A Full-Stack Web Developer , writing clean and maintainable code,with a focus on full-stack development. </p><br></br>
-        <img src={linkedin} id="linkedin-png" className="png-home"/>
-        <img src={github} id="github-png" className="png-home"/>
-        <img src={youtube} id="" className="png-home"/>
+        <a href="https://www.linkedin.com/in/jadhav-dipali/" target="_blank"><img src={linkedin} id="linkedin-png" className="png-home"/></a> 
+        <a href="https://github.com/jadhav-dipali" target="_blank"><img src={github} id="github-png" className="png-home"/> </a>
+        <a href="https://www.youtube.com/channel/UC3vy9g2lPDJt0RnnScNTVKQ" target="_blank"><img src={youtube} id="" className="png-home"/></a> 
     </div>
     <div id="right-home">
     <img src={emojiAttitude} className="png-attitude" />
